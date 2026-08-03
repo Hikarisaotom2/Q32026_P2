d1170364dc7c09b09daba99e72fe2a5ec6f17014
Claudia Cortes <claudiacortes@Claudias-MacBook-Pro.local>
2026-08-03 17:23:32 -0600

    Clase jueves 30 de julio :clases y mutadores

--- Semana 2/clase_27072026/src/clase_27072026/Clase_27072026.java
+++ Semana 2/clase_27072026/src/clase_27072026/Clase_27072026.java
@@ -4,6 +4,7 @@
  */
 package clase_27072026;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -20,37 +21,91 @@ public class Clase_27072026 {
 //        claudia.nombre="Claudia";
 //        claudia.edad=89;
 //        claudia.estatura=170;
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("ingrese el nombre");
-        String nombre = entrada.nextLine();
-     Persona edwin = new Persona(nombre, 20, 172);
-        System.out.println("La informacion de edwin");
+//        Scanner entrada = new Scanner(System.in);
+//        System.out.println("ingrese el nombre");
+//        String nombre = entrada.nextLine();
+//     Persona edwin = new Persona(nombre, 20, 172);
+//      Persona carlos = edwin ;
+//      
+//      edwin.actualizarNombre("Josue");
+//      
+//      
+//        System.out.println(edwin.verNombre());
+//        System.out.println(carlos.verNombre());
+  
         
-        System.out.println("Usando to string automatico");
-        System.out.println(edwin);
-         System.out.println("Llamando to string");
-        System.out.println(edwin.toString());
+//        System.out.println("La informacion de edwin");
+        
+//        System.out.println("Usando to string automatico");
+//        System.out.println(edwin);
+//         System.out.println("Llamando to string");
+//        System.out.println(edwin.toString());
              
     
-        
+
+    /*crud
+
+    -> crear personas 
+    -> listar personas
+    -> buscar personas
+    -> eliminar personas 
+    -> actualizar personas*/
+    
+    ArrayList <Persona> personas = new ArrayList <Persona>();
+  
+//    personas.add(crearPersona());
 
     
+//    Persona resultado = crearPersona();
+//    personas.add(resultado);
+        
+
+   personas.add(new Persona(20,"Carlos",185));
+   personas.add(new Persona(18,"Daniela",155));
+   personas.add(new Persona(18,"Isvetny",170));
+   imprimirPersonas(personas);
+   
     }
     
-    void saludar(){
+    
+    public static void imprimirPersonas(ArrayList <Persona> personas){
+        
+//        for (int i = 0; i < personas.size(); i++) {
+//            Persona persona = personas.get(i);
+//        }
+        
+        for (Persona persona : personas) {
+            System.out.println(persona);
+        }
         
     }
     
-    void saludar(int x){
+    public static void buscarPersonas(ArrayList <Persona> personas, String nombre){
         
+        for (int i = 0; i <personas.size(); i++) {
+            
+            if(nombre.equalsIgnoreCase( personas.get(i).getNombre())){
+//        if (personas.contains(new Persona(0,nombre,170))) { no funciona por el hascode de cada objeto 
+                System.out.println("Se encontro el objeto");
+                break;
+                
+            }
+            
+        }
+    }
+    public static  Persona crearPersona(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Nombre de la persona");
+        String nombre = entrada.nextLine();
+        System.out.println("ingrese la edad: ");
+        int edad = entrada.nextInt();
+        System.out.println("Ibgrese la estatura");
+        double estatura = entrada.nextDouble();
+        
+        Persona nuevaPersona = new Persona(edad,nombre,estatura);
+        return new Persona(edad,nombre,estatura);
+//        return nuevaPersona;
     }
     
-    
-      void saludar(String valor2, int x){
-        
-    }
-      
-            void saludar(int x, String valor2){
-        
-    }
+   
 }
--- Semana 2/clase_27072026/src/clase_27072026/Persona.java
+++ Semana 2/clase_27072026/src/clase_27072026/Persona.java
@@ -29,35 +29,45 @@ public class Persona {
         // Constructor 
     // 1) no tiene tipo de retorno 
     // 2) debe llamarse igual que la clase 
-     Persona(){
-      
-    } 
-    
-    Persona(String nombre, int edad, double estatura){
-        // this 
+
+    public Persona(int edad, String nombre, double estatura) {
         this.edad = edad;
         this.nombre = nombre;
-        this.estatura= estatura;
-    } // fin del constructor 
-    
-    
-    
-    // NO SE PUEDE MODIFICAR SU NOMBRE NI PARAMETROS........
-    
-    
-    // getters y setters 
-    // getter: es un metodo que me ayuda a obtener el valor de una variable  // me ayuda a la lectura 
-    public String verNombre(){
+        this.estatura = estatura;
+    }
+
+     // NO SE PUEDE MODIFICAR SU NOMBRE NI PARAMETROS........
+    // getters y setters
+    // getter: es un metodo que me ayuda a obtener el valor de una variable  // me ayuda a la lectura
+    // setter: es un metodo que me ayuda a actualizar el valor de una variable. // me ayuda a la escritura
+
+    public int getEdad() {
+      
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
         return nombre;
     }
-    // setter: es un metodo que me ayuda a actualizar el valor de una variable. // me ayuda a la escritura 
-    public void actualizarNombre(String nombre){
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
     }
 
     @Override
     public String toString() {
-      
         return "Nombre: "+this.nombre;
     }
     
